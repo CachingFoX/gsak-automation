5b41642a1e632ddc08e8ed26dd440bfeb696d6ba
CachingFoX <cachingfox@cachingfox.org>
2023-01-04 09:32:25 +0100

    build valid gskz

--- GC5PK8F-NüCC_Nürnberg_City_Challenge/build.sh
+++ GC5PK8F-NüCC_Nürnberg_City_Challenge/build.sh
@@ -1,3 +1,10 @@
 #!/bin/zsh
-rm -f NürnbergCityChallenge.gskz
-zip -r NürnbergCityChallenge.gskz assets/*.polygon NürnbergCityChallenge.gsk >build.log
+rm -f NuernbergCityChallenge.gskz
+rm -f build.log
+
+cp gskz.prototype NuernbergCityChallenge.gskz
+zip -r NuernbergCityChallenge.gskz NuernbergCityChallenge.gsk >>build.log
+cd assets
+zip -r ../NuernbergCityChallenge.gskz * >>../build.log
+cd ..
+unzip -l NuernbergCityChallenge.gskz >>build.log
