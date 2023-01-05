4103c4ea1cff95218b1e0e5c56708768905a4f71
CachingFoX <cachingfox@cachingfox.org>
2023-01-05 09:19:11 +0100

    first version

---
+++ NuernbergCityChallenge/gskz.py
@@ -0,0 +1,10 @@
+import zipfile
+
+def create_gskz( gsk_filename ):
+  with zipfile.ZipFile(f"{gsk_filename}z_", 'w', zipfile.ZIP_DEFLATED) as zipf:
+    zipf.comment = f"{gsk_filename}".encode('windows-1250')+ b"\xfeUnknown\xfeUnknown\xfe\xfe"
+
+
+if __name__ == "__main__":
+  create_gskz("NuernbergCityChallenge.gsk")
+  
