79f1c8bd05ed64f6cab96a66af4961671f24b5ef
CachingFoX <cachingfox@cachingfox.org>
2023-01-06 13:41:57 +0100

    check code with flake8

--- NuernbergCityChallenge/gskz.py
+++ NuernbergCityChallenge/gskz.py
@@ -2,25 +2,23 @@ import os
 import zipfile
 
 
-"""
-Creates a zip archive includes
-  - comment with the name of the macro file (gsk file)
-  - the macro file (gsk file)
-  - additional files from the directory
-"""
-def create_gskz( gsk_filename, folder_path ):
-  
+def create_gskz(gsk_filename, folder_path):
+  """
+  Creates a zip archive includes
+    - comment with the name of the macro file (gsk file)
+    - the macro file (gsk file)
+    - additional files from the directory
+  """
   with zipfile.ZipFile(f"{gsk_filename}z", 'w', zipfile.ZIP_DEFLATED) as zipf:
     zipf.write(f"{gsk_filename}")
-    zipf.comment = f"{gsk_filename}".encode('windows-1250')+ b"\xfeUnknown\xfeUnknown\xfe\xfe"
+    zipf.comment = f"{gsk_filename}".encode('windows-1250') + b"\xfeUnknown\xfeUnknown\xfe\xfe"
 
     len_dir_path = len(folder_path)
     for root, _, files in os.walk(folder_path):
       for file in files:
         file_path = os.path.join(root, file)
-        zipf.write(file_path, file_path[len_dir_path:])    
-    
+        zipf.write(file_path, file_path[len_dir_path:])
+
 
 if __name__ == "__main__":
   create_gskz("NuernbergCityChallenge.gsk", "./assets")
-  
